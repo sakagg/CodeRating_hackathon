764dd4f5af274a539173100fa0d75acb9b3af4f0
Saksham Aggarwal <s.agg@live.com>
2014-05-03 09:35:35 +0530

    First Commit

---
+++ template_footer.php
@@ -0,0 +1,6 @@
+ <!-- FOOTER -->
+<div id="footer">
+  <div class="container">
+    <p class="muted credit">CodeRating.com<span style="align: right; float: right;">Follow on FB</span></p>
+  </div>
+</div>
